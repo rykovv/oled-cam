35dbe0044046a513ef13bd4238f980036a7ef814
Vladislav Rykov <vladoss@list.ru>
2021-06-29 12:35:20 -0700

    camera config

--- src/main.cpp
+++ src/main.cpp
@@ -40,10 +40,56 @@ OneButton button(BUTTON_1, true);
 
 void setup() {
     oled.init();
+    oled.setFont(ArialMT_Plain_16);
+    oled.setTextAlignment(TEXT_ALIGN_CENTER);
     delay(50);
 
     oled.setPixel(10, 10);
     oled.display();
+
+    camera_config_t config;
+    config.ledc_channel = LEDC_CHANNEL_0;
+    config.ledc_timer = LEDC_TIMER_0;
+    config.pin_d0 = Y2_GPIO_NUM;
+    config.pin_d1 = Y3_GPIO_NUM;
+    config.pin_d2 = Y4_GPIO_NUM;
+    config.pin_d3 = Y5_GPIO_NUM;
+    config.pin_d4 = Y6_GPIO_NUM;
+    config.pin_d5 = Y7_GPIO_NUM;
+    config.pin_d6 = Y8_GPIO_NUM;
+    config.pin_d7 = Y9_GPIO_NUM;
+    config.pin_xclk = XCLK_GPIO_NUM;
+    config.pin_pclk = PCLK_GPIO_NUM;
+    config.pin_vsync = VSYNC_GPIO_NUM;
+    config.pin_href = HREF_GPIO_NUM;
+    config.pin_sscb_sda = SIOD_GPIO_NUM;
+    config.pin_sscb_scl = SIOC_GPIO_NUM;
+    config.pin_pwdn = PWDN_GPIO_NUM;
+    config.pin_reset = RESET_GPIO_NUM;
+    config.xclk_freq_hz = 20000000;
+    config.pixel_format = PIXFORMAT_RGB888;
+    config.frame_size = FRAMESIZE_QQVGA;
+    config.jpeg_quality = 10;
+    config.fb_count = 2;
+
+    /* camera sensor init */
+    esp_err_t err = esp_camera_init(&config);
+    if (err != ESP_OK) {
+        ESP_LOGE(TAG, "Camera init Fail");
+        oled.clear();
+        oled.drawString(oled.getWidth() / 2, oled.getHeight() / 2, "Camera init Fail");
+        oled.display();
+
+        while (1);
+    }
+
+    sensor_t *s = esp_camera_sensor_get();
+    /* Flip image initially if necessary */
+    // s->set_vflip(s, 1);
+    /* Black and white special effect set */
+    s->set_special_effect(s, 2);
+
+    button.attachClick(button_func);
 }
 
 void loop() {
